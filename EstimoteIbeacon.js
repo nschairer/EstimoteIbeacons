b450eb0eb96baf5850fd7792799d3db29863b444
nschairer <nschairer@gmail.com>
2018-04-20 11:59:15 -0400

    Add files via upload

---
+++ EstimoteIbeacon.js
@@ -0,0 +1,21 @@
+
+
+var fs = require('fs');
+var Bleacon = require('bleacon');
+var UUID = 'your UUID';
+var Major = 'Your Major';
+Bleacon.startScanning(UUID,Major); // scan for ibeacons
+var jsonArray =[];
+Bleacon.on('discover', function(bleacon) {
+        //bleacon properties are: uuid, major, minor, measuredPower, rssi, accu$
+                var beacon = JSON.stringify(bleacon);
+                jsonArray.push(bleacon)
+                fs.writeFile("test.json",JSON.stringify(jsonArray), function(er$
+                if (err) {
+                        return console.log(err);
+                }
+                console.log('file saved')
+        });
+                console.log(beacons[bleacon.minor],bleacon.rssi)
+
+});
